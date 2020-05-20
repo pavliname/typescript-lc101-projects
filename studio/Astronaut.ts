e067ac2c47cb0e150c97d0da87ed2f3c49feef2b
Pavlina <pavlna@mail.usf.edu>
2020-05-20 15:29:25 +0200

    Typescript studio completed

---
+++ studio/Astronaut.ts
@@ -0,0 +1,11 @@
+import { Payload } from "./Payload";
+​
+export class Astronaut implements Payload {
+  name: string;
+  massKg: number;
+​
+  constructor(massKg: number, name: string) {
+    this.name = name;
+    this.massKg = massKg;
+  }
+}
